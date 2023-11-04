82d43ec6962665119b72186363bd90557b718c4f
ShivamVikash <131040493+ShivamVikash@users.noreply.github.com>
2023-11-04 15:50:12 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <input type="hidden" value="21BCE10949"/>
+    <input type="hidden" value="https://github.com/Vimall03 - follow me on github" />
+  </React.StrictMode>
+);
